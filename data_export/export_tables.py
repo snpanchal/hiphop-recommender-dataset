d7f3f5ebd2818e492f25eb96a072cd1689e64c65
Shyam Panchal <snpanchal44@gmail.com>
2021-04-28 20:52:41 -0600

    Add SQL script to create psql tables

--- data_export/export_tables.py
+++ data_export/export_tables.py
@@ -5,18 +5,18 @@ db_con = sqlite3.connect('../albums.db')
 
 # Export data in albums table
 df_albums = pd.read_sql_query('SELECT * FROM albums', db_con)
-df_albums.to_csv(path_or_buf='Albums.csv', index=False)
+df_albums.to_csv(path_or_buf='../create_psql_db/Albums.csv', index=False)
 
 # Export data in Artists table
 df_artists = pd.read_sql_query('SELECT * FROM Artists', db_con)
-df_artists.to_csv(path_or_buf='Artists.csv', index=False)
+df_artists.to_csv(path_or_buf='../create_psql_db/Artists.csv', index=False)
 
 # Export data in AlbumArtists table
 df_album_artists = pd.read_sql_query('SELECT * FROM AlbumArtists', db_con)
-df_album_artists.to_csv(path_or_buf='AlbumArtists.csv', index=False)
+df_album_artists.to_csv(path_or_buf='../create_psql_db/AlbumArtists.csv', index=False)
 
 # Export data in album_features table
 df_album_features = pd.read_sql_query('SELECT * FROM album_features', db_con)
-df_album_features.to_csv(path_or_buf='AlbumFeatures.csv', index=False)
+df_album_features.to_csv(path_or_buf='../create_psql_db/AlbumFeatures.csv', index=False)
 
 db_con.close()
